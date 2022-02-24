58258b428b7e13a93e6e0276db1c730ad8fe46f4
sravankilari91 <killar.srav143@gmail.com>
2022-02-24 15:54:14 +0530

    Inital Commit

---
+++ User/User/src/test/java/com/sravan/gqlfederation/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravan.gqlfederation.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
